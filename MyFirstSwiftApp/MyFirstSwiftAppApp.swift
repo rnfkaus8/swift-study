0e3203cc9ad00b4ec5439d890d4af4ee73a8fcbb
kimtaehyun-minding <kth4350@minding.today>
2022-11-01 21:06:03 +0900

    Initial commit

---
+++ MyFirstSwiftApp/MyFirstSwiftAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  MyFirstSwiftAppApp.swift
+//  MyFirstSwiftApp
+//
+//  Created by 김태현 on 2022/11/01.
+//
+//
+
+import SwiftUI
+
+@main
+struct MyFirstSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
