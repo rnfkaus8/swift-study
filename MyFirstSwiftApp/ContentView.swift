4d9ee69d9ba8eda8628f724172817d1abc260f54
rnfkaus8 <kth4350@gmail.com>
2022-11-09 19:07:38 +0900

    add Compose the Detail View

--- MyFirstSwiftApp/ContentView.swift
+++ MyFirstSwiftApp/ContentView.swift
@@ -13,18 +13,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Title Text View")
-                    .font(.title)
-            HStack {
-                Text("Subheadline Text View")
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                    .frame(height: 300)
+
+            CatImageView()
+                    .offset(y: -130)
+                    .padding(.bottom, -120)
+
+            VStack(alignment: .leading) {
+                Text("Title Text View")
+                        .font(.title)
+                HStack {
+                    Text("Subheadline Text View")
+                    Spacer()
+                    Text("use VStack")
+                }
                         .font(.subheadline)
-                Spacer()
-                Text("use VStack")
-                        .font(.body)
+                        .foregroundColor(.secondary)
+
+                Divider()
+
+                Text("About Cat")
+                        .font(.title2)
+                Text("Cat is so much cute")
             }
+                    .padding()
+
+            Spacer()
         }
-        .padding()
     }
 }
 
